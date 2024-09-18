c468915465dcc4e2363b5601110f1184868a3981
Phnks Invictus <phnksinvictus@gmail.com>
2024-09-18 19:59:03 -0400

    fix game constant

--- Assets/Scripts/GameConstants.cs
+++ Assets/Scripts/GameConstants.cs
@@ -1,5 +1,5 @@
 public static class GameConstants
 {
-    public static readonly float MOVEMENT_CONVERSION_FACTOR = 33.03f; // Conversion factor for movement and range
+    public static readonly float MOVEMENT_CONVERSION_FACTOR = 25.4f; // Conversion factor for movement and range
 }
 
